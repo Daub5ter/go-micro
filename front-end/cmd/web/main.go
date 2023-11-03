26ed61e7ad0e25c7144fe3fb05c11bdf48d7dbc2
Daub5ter <tsmaks2@gmail.com>
2023-11-03 22:29:26 +0300

    create the Caddy proxy

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,29 +13,34 @@ func main() {
 		render(w, "test.page.html")
 	})
 
-	fmt.Println("Starting front end service on port 1234")
-	err := http.ListenAndServe(":1234", nil)
+	fmt.Println("Starting front end service on port 8081")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
+//go:embed templates
+var templateFS embed.FS
+
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.html",
-		"./cmd/web/templates/header.partial.html",
-		"./cmd/web/templates/footer.partial.html",
+		//"./cmd/web/templates/base.layout.html",
+		"templates/base.layout.html",
+		"templates/header.partial.html",
+		"templates/footer.partial.html",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	//./cmd/web/templates/
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
