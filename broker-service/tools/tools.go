15a865239c19e6b4fd28e2d40e53ed22460ced3c
Daub5ter <tsmaks2@gmail.com>
2023-11-17 17:40:55 +0300

    updated tools for random seed

--- broker-service/tools/tools.go
+++ broker-service/tools/tools.go
@@ -1,6 +1,9 @@
 package tools
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func RandomString(l int) string {
 	bytes := make([]byte, l)
@@ -12,5 +15,6 @@ func RandomString(l int) string {
 }
 
 func randInt(min, max int) int {
-	return min + rand.Intn(max-min)
+	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return min + generator.Intn(max-min)
 }
