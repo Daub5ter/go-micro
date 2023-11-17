ed82dd273116ab2cf6151a6d3ce8ebdc39062a13
Daub5ter <tsmaks2@gmail.com>
2023-11-17 15:38:54 +0300

    created client of rpc rabbitmq

--- broker-service/event/emtter.go
+++ broker-service/event/emtter.go
@@ -1,7 +1,9 @@
 package event
 
 import (
+	"broker/tools"
 	"context"
+	"encoding/json"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 )
@@ -45,6 +47,59 @@ func (e *Emitter) Push(event string, exchange string, severity string) error {
 	}
 
 	return nil
+}
+
+func (e *Emitter) PushWithResponse(event string, exchange string, severity string) (string, error) {
+	payload := struct {
+		Error error
+		Data  string
+	}{}
+
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return "", err
+	}
+	defer channel.Close()
+
+	q, err := declareRandomQueue(channel)
+
+	msgs, err := channel.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return "", err
+	}
+
+	corrID := tools.RandomString(32)
+
+	log.Println("Pushing to channel")
+
+	err = channel.PublishWithContext(
+		context.TODO(),
+		exchange,
+		severity,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrID,
+			ReplyTo:       q.Name,
+			Body:          []byte(event),
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	for d := range msgs {
+		if corrID == d.CorrelationId {
+			_ = json.Unmarshal(d.Body, &payload)
+			if err != nil {
+				return "", err
+			}
+			break
+		}
+	}
+
+	return payload.Data, payload.Error
 }
 
 func NewEventEmitter(name string, conn *amqp.Connection) (Emitter, error) {
